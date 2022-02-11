2922b6725f64b2d6dac79533f20e2befea960373
Rafał Rybicki <rafarybicki@gmail.com>
2022-02-11 19:39:39 +0100

    Add types

---
+++ types.tsx
@@ -0,0 +1,35 @@
+export type Room = {
+  id: string;
+  name: string;
+  messages?: Message[];
+  user?: User;
+}
+
+export type RoomData = {
+  room: Room
+}
+
+export type UsersRooms = {
+  usersRooms: {
+    rooms: Room[];
+  }
+}
+
+export type Rooms = {
+  rooms: Room[];
+}
+
+type Message = {
+  id: string;
+  body: string;
+  insertedAt: string;
+  user: User;
+}
+
+type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email?: string;
+  role?: string;
+}
